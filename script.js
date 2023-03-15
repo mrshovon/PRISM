57852231bea1d9a71d9e5e025292262480ade18c
Mushfiq <1730020@iub.edu.bd>
2023-03-15 16:49:40 +0600

    15323/449/ogg/lap

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function myFunction() {
+    var element = document.getElementById("hidden");
+    element.classList.toggle("menu-items");
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("hidden");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
