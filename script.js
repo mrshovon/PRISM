320700883851201f240aaf691879d5ab7fe05753
Mushfiq <1730020@iub.edu.bd>
2023-03-19 15:06:01 +0600

    adasd

--- script.js
+++ script.js
@@ -9,8 +9,16 @@
   function myFunction3() {
     var x = document.getElementById("services");
     if (x.style.display === "none") {
-      x.style.display = "block";
+       x.style.display = "block";
+       x.style.backgroundColor = "white";
     } else {
       x.style.display = "none";
+    }
+  }
+
+  function searchResult(){
+    
+    for(i=1;i<=10;i++){
+
     }
   }
