efb592dd3c41ff548e44a3bb2754dfae75fa4282
Mushfiq <1730020@iub.edu.bd>
2023-03-20 13:00:13 +0600

    asd

--- script.js
+++ script.js
@@ -17,8 +17,23 @@
   }
 
   function searchResult(){
-    
+    const sr = document.getElementById('searchResult');
+    sr.innerHTML= '';
     for(i=1;i<=10;i++){
-
+      sr.innerHTML += `<div class="search-main" >
+        <div style="margin-left: 27px; margin-top: 9px;">
+          <img src="https://www.timeoutdubai.com/cloud/timeoutdubai/2021/09/14/yvA5SpUH-IMG-Worlds-1200x900.jpg" alt="" style="width:200px; height: 180px; border-radius: 0px;"> 
+        </div>
+        <div style=" margin-top: 9px;margin-left: 19px;">
+          <h3>Price: xxxxxxxx </h3>
+          <h6>Property-type: appartment </h6>
+          <h6>Property-type: residential</h6>
+          <h6>size: xx sqft  </h6>
+          <h6>bed:x </h6>
+          <h6>baths:x </h6>
+          <a href="property-details.html" class="btn btn-info" style="position: relative; position: relative;bottom: 33px;left: 168px;">Details</a>
+        </div>
+      </div>`;
     }
   }
+
